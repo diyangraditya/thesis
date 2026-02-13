a747f26b9f42284f2ee38cad0f2135091fef14c8
diyangraditya <diyangraditya144@gmail.com>
2026-02-13 22:56:44 +0700

    refactor: step by step upgrade from listing tech owner knowledge

---
+++ Python/config.py
@@ -0,0 +1,32 @@
+TECH_OWNER_KNOWLEDGE = {
+    "INFRA": {
+        "full_name": "Infrastructure Division",
+        "description": "",
+        "products_handled": [""]
+    },
+    "DIP": {
+        "full_name": "",
+        "description": "",
+        "products_handled": [""]
+    },
+    "DLA": {
+        "full_name": "",
+        "description": "",
+        "products_handled": [""]
+    },
+    "DPP": {
+        "full_name": "",
+        "description": "",
+        "products_handled": [""]
+    },
+    "Minova": {
+        "full_name": "",
+        "description": "",
+        "products_handled": [""]
+    },
+    "DAK": {
+        "full_name": "",
+        "description": "",
+        "products_handled": [""]
+    }
+}
