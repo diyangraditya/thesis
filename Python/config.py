ed12bc9936207e90e47f24135c6dae1661a48dd0
diyangraditya <diyangraditya144@gmail.com>
2026-02-14 16:34:32 +0700

    chore: updating build thesis tasks

--- Python/config.py
+++ Python/config.py
@@ -2,31 +2,38 @@ TECH_OWNER_KNOWLEDGE = {
     "INFRA": {
         "full_name": "Infrastructure Division",
         "description": "",
-        "products_handled": [""]
+        "products_handled": "",
+        "projects_handled": ["SIMPKB", "SIMPATIKA", "PerlindunganPTK", "SekolahPenggerak", "BELMAWA", "DiklatDIKMENDIKSUS", "infra", "PPG", "Inkubasi", "GBGTKPAUDDB", "GPO", "PPGMUTU", "P2GTKMP", "TAMASKA", "DIKLATKSPS", "GuruPenggerak", "BACKUP", "CDN", "praktik-baik", "PBS", "PPGPRAJAB", "dio", "LMSERKAM", "UKKJ", "SMARTVILLAGE", "GPK", "PPDB-Online", "PSP-DIKLAT-IKM-PS", "MASOOK", "GBGTKPAUD", "KEMENSOS", "SmartCityNavigator", "PPGMUTUPPKS", "PPB", "MONEV", "UKKT", "eparkir", "GBP3K", "PERSONALPTK"]
     },
     "DIP": {
         "full_name": "",
         "description": "",
-        "products_handled": [""]
+        "products_handled": "",
+        "projects_handled": ["PerlindunganPTK", "CDN", "SekolahPenggerak", "PPG", "SIMPKB", "smkpk-rekognisi", "DIKLATKSPS", "DiklatDIKMENDIKSUS", "PPGPRAJAB", "Inkubasi", "GuruPenggerak", "PPGMUTU", "P2GTKMP", "UKKJ", "GBGTKPAUDDB", "praktik-baik", "SIMPATIKA", "PERSONALPTK"]
     },
     "DLA": {
         "full_name": "",
         "description": "",
-        "products_handled": [""]
+        "products_handled": [""],
+        "projects_handled": ["PPG", "GBGTKPAUDDB", "SIMPKB", "SIMPATIKA", "PPGPRAJAB", "Inkubasi", "GuruPenggerak", "SekolahPenggerak", "P2GTKMP", "infra"]
     },
     "DPP": {
         "full_name": "",
         "description": "",
-        "products_handled": [""]
+        "products_handled": [""],
+        "projects_handled": ["SIMPKB", "DiklatDIKMENDIKSUS", "SIMPATIKA", "MASOOK", "PPG", "Masook"]
     },
     "Minova": {
         "full_name": "",
         "description": "",
-        "products_handled": [""]
+        "products_handled": [""],
+        "projects_handled": ["GuruPenggerak", "UKKJ", "P2GTMP"]
     },
     "DAK": {
         "full_name": "",
         "description": "",
-        "products_handled": [""]
+        "products_handled": [""],
+        "projects_handled": ["infra"]
     }
 }
+#%%
