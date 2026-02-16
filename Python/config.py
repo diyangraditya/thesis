b7781bf9d18051393323cc38020e0995007aa875
diyangraditya <diyangraditya144@gmail.com>
2026-02-16 22:47:59 +0700

    chore: continuing develop context

--- Python/config.py
+++ Python/config.py
@@ -2,37 +2,37 @@ TECH_OWNER_KNOWLEDGE = {
     "INFRA": {
         "full_name": "Infrastructure Division",
         "description": "",
-        "products_handled": "",
+        "used_operation": ["CreateVolume-Gp3", "CreateVolume", "RunInstances", "EBS:IO-Write", "PublicIP-In", "GET", "GlacierInstantRetrievalStorage", "CreateVolume-Sc1", "InterZone-In", "Hourly", "EBS:IO-Read", "ReadCostAllocation", "PublicIP-Out", "GetObject", "CreateVolume-St1", "HeadBucket", "LoadBalancing:Network", "StandardStorage", "POST", "OPTIONS", "InterZone-Out", "DeepArchiveS3ObjectOverhead", "ObjectTagCount", "PutObject", "OneZoneIASizeOverhead", "OneZoneIAStorage", "DELETE", "ReadACL", "HEAD", "PUT", "GlacierStorage", "DeepArchiveStagingStorage", "GlacierObjectOverhead", "GlacierS3ObjectOverhead", "IntelligentTieringAIAStorage", "HeadObject", "DeleteObject", "PreflightRequest", "CreateSnapshot", "ReadLocation", "ReadBucketPublicAccessBlock", "ReadBucketPolicyStatus", "ReadBucketPolicy", "UploadPart", "ListBucket", "InitiateMultipartUpload", "CompleteMultipartUpload", "ReadLogProps", "ReadVersioningProps", "ReadAccelerate", "ReadBucketOwnershipControls", "ReadBucketIntelligentTiering", "ReadBucketServerSideEncryption", "ReadRequestPaymentProps", "ReadNotificationProps", "WriteCostAllocation", "ReadBucketCors", "ReadBucketLifecycle", "ReadBucketInventory", "ReadBucketAnalytics", "ReadObjectTagging", "ReadBucketReplication"],
         "projects_handled": ["SIMPKB", "SIMPATIKA", "PerlindunganPTK", "SekolahPenggerak", "BELMAWA", "DiklatDIKMENDIKSUS", "infra", "PPG", "Inkubasi", "GBGTKPAUDDB", "GPO", "PPGMUTU", "P2GTKMP", "TAMASKA", "DIKLATKSPS", "GuruPenggerak", "BACKUP", "CDN", "praktik-baik", "PBS", "PPGPRAJAB", "dio", "LMSERKAM", "UKKJ", "SMARTVILLAGE", "GPK", "PPDB-Online", "PSP-DIKLAT-IKM-PS", "MASOOK", "GBGTKPAUD", "KEMENSOS", "SmartCityNavigator", "PPGMUTUPPKS", "PPB", "MONEV", "UKKT", "eparkir", "GBP3K", "PERSONALPTK"]
     },
     "DIP": {
         "full_name": "",
         "description": "",
-        "products_handled": "",
+        "used_operation": ["InterZone-Out", "CreateVolume", "Hourly", "RunInstances", "EBS:IO-Write", "InterZone-In", "RunInstances:SV003", "EBS:IO-Read", "RunInstances:SV002", "HeadBucket", "PublicIP-Out", "CreateVolume-Gp3", "PublicIP-In", "ReadCostAllocation", "StandardStorage", "ReadACL", "ReadBucketPolicyStatus", "ReadLocation", "ReadBucketPolicy", "ReadBucketPublicAccessBlock", "ListBucket", "CreateSnapshot", "T4GCPUCredits", "ReadVersioningProps", "ReadBucketOwnershipControls", "GetObject"],
         "projects_handled": ["PerlindunganPTK", "CDN", "SekolahPenggerak", "PPG", "SIMPKB", "smkpk-rekognisi", "DIKLATKSPS", "DiklatDIKMENDIKSUS", "PPGPRAJAB", "Inkubasi", "GuruPenggerak", "PPGMUTU", "P2GTKMP", "UKKJ", "GBGTKPAUDDB", "praktik-baik", "SIMPATIKA", "PERSONALPTK"]
     },
     "DLA": {
         "full_name": "",
         "description": "",
-        "products_handled": [""],
+        "used_operation": [""],
         "projects_handled": ["PPG", "GBGTKPAUDDB", "SIMPKB", "SIMPATIKA", "PPGPRAJAB", "Inkubasi", "GuruPenggerak", "SekolahPenggerak", "P2GTKMP", "infra"]
     },
     "DPP": {
         "full_name": "",
         "description": "",
-        "products_handled": [""],
+        "used_operation": [""],
         "projects_handled": ["SIMPKB", "DiklatDIKMENDIKSUS", "SIMPATIKA", "MASOOK", "PPG", "Masook"]
     },
     "Minova": {
         "full_name": "",
         "description": "",
-        "products_handled": [""],
+        "used_operation": [""],
         "projects_handled": ["GuruPenggerak", "UKKJ", "P2GTMP"]
     },
     "DAK": {
         "full_name": "",
         "description": "",
-        "products_handled": [""],
+        "used_operation": [""],
         "projects_handled": ["infra"]
     }
 }
