d84918e881ceb0cdfcc1ffa24cc1b36826a3e35d
diyangraditya <diyangraditya144@gmail.com>
2026-02-24 18:35:06 +0700

    chore: finalize the data valuess and the conf

--- Python/config.py
+++ Python/config.py
@@ -4,7 +4,7 @@ TECH_OWNER_KNOWLEDGE = {
         "description": "Mengelola infrastruktur dan layanan IT perusahaan",
         "scope": "Jaringan, server & cloud, keamanan sistem, maintenance perangkat, backup & monitoring sistem",
         "used_operation": ["CreateVolume-Gp3", "CreateVolume", "RunInstances", "EBS:IO-Write", "PublicIP-In", "GET", "GlacierInstantRetrievalStorage", "CreateVolume-Sc1", "InterZone-In", "Hourly", "EBS:IO-Read", "ReadCostAllocation", "PublicIP-Out", "GetObject", "CreateVolume-St1", "HeadBucket", "LoadBalancing:Network", "StandardStorage", "POST", "OPTIONS", "InterZone-Out", "DeepArchiveS3ObjectOverhead", "ObjectTagCount", "PutObject", "OneZoneIASizeOverhead", "OneZoneIAStorage", "DELETE", "ReadACL", "HEAD", "PUT", "GlacierStorage", "DeepArchiveStagingStorage", "GlacierObjectOverhead", "GlacierS3ObjectOverhead", "IntelligentTieringAIAStorage", "HeadObject", "DeleteObject", "PreflightRequest", "CreateSnapshot", "ReadLocation", "ReadBucketPublicAccessBlock", "ReadBucketPolicyStatus", "ReadBucketPolicy", "UploadPart", "ListBucket", "InitiateMultipartUpload", "CompleteMultipartUpload", "ReadLogProps", "ReadVersioningProps", "ReadAccelerate", "ReadBucketOwnershipControls", "ReadBucketIntelligentTiering", "ReadBucketServerSideEncryption", "ReadRequestPaymentProps", "ReadNotificationProps", "WriteCostAllocation", "ReadBucketCors", "ReadBucketLifecycle", "ReadBucketInventory", "ReadBucketAnalytics", "ReadObjectTagging", "ReadBucketReplication"],
-        "products_handled": ["Storage", "Data Transfer", "Compute Instance", "System Operation", "Request", "API Request", "Load Balancer-Network", "Fee", "Storage Snapshot"],
+        "products_handled": ["Storage", "Data Transfer", "Compute Instance", "System Operation", "Request", "API Request", "Load Balancer-Network", "Storage Snapshot"],
         "projects_handled": ["SIMPKB", "SIMPATIKA", "PerlindunganPTK", "SekolahPenggerak", "BELMAWA", "DiklatDIKMENDIKSUS", "infra", "PPG", "Inkubasi", "GBGTKPAUDDB", "GPO", "PPGMUTU", "P2GTKMP", "TAMASKA", "DIKLATKSPS", "GuruPenggerak", "BACKUP", "CDN", "praktik-baik", "PBS", "PPGPRAJAB", "dio", "LMSERKAM", "UKKJ", "SMARTVILLAGE", "GPK", "PPDB-Online", "PSP-DIKLAT-IKM-PS", "MASOOK", "GBGTKPAUD", "KEMENSOS", "SmartCityNavigator", "PPGMUTUPPKS", "PPB", "MONEV", "UKKT", "eparkir", "GBP3K", "PERSONALPTK"]
     },
     "DIP": {
@@ -19,7 +19,7 @@ TECH_OWNER_KNOWLEDGE = {
         "full_name": "Divisi Layanan Data dan Analitik",
         "description": "Mengelola data dan menghasilkan insight berbasis data",
         "scope": "Pemrosesan data, dashboard & reporting, analisis bisnis, AI/Machine Learning, data quality",
-        "used_operation": ["Not Applicable", "CreateDBInstance:0018", "InterZone-In", "CreateDBInstance:0016", "InterZone-Out", "RunInstances", "Hourly", "CreateVolume", "CreateVolume-Gp3", "EBS:IO-Write", "EBS:IO-Read", "CreateDBInstance", "CreateSnapshot"],
+        "used_operation": ["CreateDBInstance:0018", "InterZone-In", "CreateDBInstance:0016", "InterZone-Out", "RunInstances", "Hourly", "CreateVolume", "CreateVolume-Gp3", "EBS:IO-Write", "EBS:IO-Read", "CreateDBInstance", "CreateSnapshot"],
         "products_handled": ["Data Transfer", "Database Storage", "Database Instance", "Compute Instance", "Storage", "System Operation", "Storage Snapshot"],
         "projects_handled": ["PPG", "GBGTKPAUDDB", "SIMPKB", "SIMPATIKA", "PPGPRAJAB", "Inkubasi", "GuruPenggerak", "SekolahPenggerak", "P2GTKMP", "infra"]
     },
@@ -29,12 +29,12 @@ TECH_OWNER_KNOWLEDGE = {
         "scope": "Pengembangan frontend & backend, UI/UX, integrasi API, testing, deployment, dan maintenance sistem",
         "used_operation": ["ReadCostAllocation", "CreateVolume", "ApiGatewayRequest", "HeadBucket", "StandardStorage", "EBS:IO-Read", "RunInstances", "Invoke", "InterZone-In", "Hourly", "PutLogEvents", "EBS:IO-Write", "InterZone-Out", "Shutdown", "HourlyStorageMetering", "ReadBucketPublicAccessBlock", "ReadBucketPolicy", "ReadLocation", "ReadACL", "ReadBucketPolicyStatus"],
         "products_handled": ["Data Transfer", "Storage", "API Calls", "API Request", "System Operation", "Compute Instance", "Serverless", "Data Payload", "Storage Snapshot"],
-        "projects_handled": ["SIMPKB", "DiklatDIKMENDIKSUS", "SIMPATIKA", "MASOOK", "PPG", "Masook"]
+        "projects_handled": ["SIMPKB", "DiklatDIKMENDIKSUS", "SIMPATIKA", "MASOOK", "PPG"]
     },
     "Minova": {
-        "full_name": "",
-        "description": "",
-        "scope": "",
+        "full_name": "PT Mitra Inovasi Jayantara (Vendor/Partner)",
+        "description": "Perusahaan mitra TIK yang fokus pada pengembangan layanan Software as a Service (SaaS) berskala nasional untuk sektor pendidikan dan pemerintahan",
+        "scope": "Pengembangan aplikasi cloud (SaaS), pengelolaan infrastruktur data storage, dan manajemen backup/snapshot (EBS) untuk proyek aplikasi eksternal.",
         "used_operation": ["CreateVolume", "EBS:IO-Read", "EBS:IO-Write", "CreateSnapshot"],
         "products_handled": ["Storage", "System Operation", "Storage Snapshot"],
         "projects_handled": ["GuruPenggerak", "UKKJ", "P2GTMP"]
